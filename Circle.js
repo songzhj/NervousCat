485dbb2ad50a3c2b694767950c1b6cf9afc3b8be
songzhj <songzhj@aliyun.com>
2016-03-27 12:21:23 +0800

    update

--- Circle.js
+++ Circle.js
@@ -7,29 +7,32 @@ function Circle() {
     this.setCircleType = function (type) {
         this._circleType = type;
         switch (type) {
-            case 1:
+            case Circle.TYPE_UNSELLECED:
                 this.setColor("#ccc");
                 break;
-            case 2:
+            case Circle.TYPE_SELLECED:
                 this.setColor("#f60");
                 break;
-            case 3:
+            case Circle.TYPE_CAT:
                 this.setColor("#f0f");
                 break;
         }
-    }
+    };
 
     this.setColor = function (colorString) {
         this.graphics.beginFill(colorString);
         this.graphics.drawCircle(0, 0, 25);
         this.graphics.endFill();
-    }
+    };
 
     this.getCircleType = function () {
         return this._circleType;
-    }
+    };
 
-    this.setCircleType(1);
+    this.setCircleType(Circle.TYPE_UNSELLECED);
 }
 
 Circle.prototype = new createjs.Shape();
+Circle.TYPE_UNSELLECED = 1;
+Circle.TYPE_SELLECED = 2;
+Circle.TYPE_CAT = 3;
